fdf1007c45add2406b6d3606fc7c337d6541c88b
andy <andygmb1@gmail.com>
2014-08-12 04:38:24 +0100

    updating config with new banned_streams and max_streams configuration

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 username = "username"
 password = "password"
-subreddit = "subreddit"
-wiki_stream_config = "streamconfig"
-wiki_streams = "streams"
+subreddit = "2007scapetest"
+max_stream_count = 12 # Maximum mount of livestreams to be displayed at once.
+wiki_stream_config = "streamconfig" # http://www.reddit.com/r/subreddit/wiki/streamconfig
+wiki_streams = "streams" # http://www.reddit.com/r/subreddit/wiki/streams
+wiki_bans = "banned_streams" # http://www.reddit.com/r/subreddit/wiki/banned_streams
