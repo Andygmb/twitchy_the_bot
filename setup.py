260afb37ee65b10ab6a40015c65a7c056f283c1c
andy <andygmb1@gmail.com>
2015-02-16 10:14:48 +0000

    changing wikipages to use a loop

--- setup.py
+++ setup.py
@@ -20,12 +20,15 @@ if __name__ == "__main__":
 			wikilog(r, sub, "twitchbot_error_log", "Invalid JSON in local file: default_wiki_config.json")
 
 	try:
-		r.edit_wiki_page(
-			sub, 
-			"twitchbot_config", 
-			json.dumps(config, indent=4), 
-			"Initial commit from setup.py with default config values"
-			)
+		r.edit_wiki_page(sub, "twitchbot_config", json.dumps(config, indent=4), "Initial setup from setup.py")
 	except requests.exceptions.HTTPError:
-		print "Couldn't access wiki page, reddit may be down."
+		print "Couldn't access /wiki/{}, reddit may be down.".format("twitchbot_config")
 		wikilog(r, sub, config["wikipages"]["error_log"], "Couldn't access wiki page, reddit may be down.")
+
+	for wikipage in config["wikipages"].values():
+		try:
+			r.edit_wiki_page(sub, wikipage, " ", "Initial setup from setup.py")
+
+	except requests.exceptions.HTTPError:
+		print "Couldn't access /wiki/{}, reddit may be down.".format(wikipage)
+		wikilog(r, sub, wikipage, "Couldn't access wiki page, reddit may be down.")
