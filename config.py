5e4d762ed5f951efc17112a0973b440b035147b6
andy <andygmb1@gmail.com>
2015-02-16 05:05:14 +0000

    update config.py to move wiki pages to wiki config json file

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
 username = "username"
 password = "password"
 subreddit = "subreddit"
-max_stream_count = 12 # Maximum mount of livestreams to be displayed at once.
-wiki_stream_config = "streamconfig" # http://www.reddit.com/r/subreddit/wiki/streamconfig
-wiki_streams = "streams" # http://www.reddit.com/r/subreddit/wiki/streams
-wiki_bans = "banned_streams" # http://www.reddit.com/r/subreddit/wiki/banned_streams
