898e4952e85e7a598471c425b804a689584fae72
andy <andygmb1@gmail.com>
2015-02-16 08:46:24 +0000

    sticking it setup into ifmainname

--- setup.py
+++ setup.py
@@ -4,27 +4,28 @@ import HTMLParser
 import requests
 from config import username, password, subreddit
 
-r = praw.Reddit("Twitch.tv sidebar bot for {} by /u/andygmb".format(subreddit))
-r.login(username=username, password=password)
-sub = r.get_subreddit(subreddit)
-
 def wikilog(r, subreddit, wikipage, error):
 	r.edit_wiki_page(subreddit, wikipage, error, error)
 
-with open("default_wiki_config.json", "r") as configjson:
-	try:
-		config = json.load(configjson)
-	except ValueError:
-		print "Invalid JSON in default_wiki_config.json"
-		wikilog(r, sub, "twitchbot_error_log", "Invalid JSON in default_wiki_config.json")
+if __name__ == "__main__":
+	r = praw.Reddit("Twitch.tv sidebar bot for {} by /u/andygmb".format(subreddit))
+	r.login(username=username, password=password)
+	sub = r.get_subreddit(subreddit)
 
-try:
-	r.edit_wiki_page(
-		sub, 
-		config["config"], 
-		json.dumps(wiki_config, indent=4), 
-		"Initial commit from setup.py with default config values"
-		)
-except requests.exceptions.HTTPError:
-	print "Couldn't access wiki page, reddit may be down."
-	wikilog(r, sub, "twitchbot_error_log", "Couldn't access wiki page, reddit may be down.")
+	with open("default_wiki_config.json", "r") as configjson:
+		try:
+			config = json.load(configjson)
+		except ValueError:
+			print "Invalid JSON in local file: default_wiki_config.json"
+			wikilog(r, sub, "twitchbot_error_log", "Invalid JSON in local file: default_wiki_config.json")
+
+	try:
+		r.edit_wiki_page(
+			sub, 
+			"twitchbot_config", 
+			json.dumps(config, indent=4), 
+			"Initial commit from setup.py with default config values"
+			)
+	except requests.exceptions.HTTPError:
+		print "Couldn't access wiki page, reddit may be down."
+		wikilog(r, sub, config["wikipages"]["error_log"], "Couldn't access wiki page, reddit may be down.")
