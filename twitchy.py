5345f8de3e28c61201d3a2b397b8b0686d4e162c
andy <andygmb1@gmail.com>
2015-07-13 21:55:15 +0100

    Add 'image_upload_name' config to allow custom stream image upload name (in case 'img' is already used on the subreddit) https://github.com/Andygmb/twitchy_the_bot/issues/16

--- twitchy.py
+++ twitchy.py
@@ -114,7 +114,7 @@ class configuration():
     def update_stylesheet(self):
         print "Uploading thumbnail image(s)"
         try:
-            self.subreddit.upload_image("img.png", "img", False)
+            self.subreddit.upload_image("img.png", self.config["image_upload_name"], False)
         except praw.errors.APIException:
             print "Too many images uploaded."
             self.wikilog("Too many images uploaded to the stylesheet, max of 50 images allowed.")
